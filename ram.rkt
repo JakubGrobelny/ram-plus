fe8af631a722943552a62c020a8b5bd529b05687
JakubGrobelny <300481@uwr.edu.pl>
2019-03-05 00:55:33 +0100

    Added execution

--- ram.rkt
+++ ram.rkt
@@ -57,10 +57,14 @@
   (let ([table (make-hash)])
     (for ([i (in-range 0 (vector-length program))])
       (match (vector-ref program i)
-        ((list label _ _)
-         (hash-set! table label i))
-        (_ (void))))
-    table))
+        ((cons label _)
+         (if (label? label)
+             (begin
+               (hash-set! table label i)
+               (vector-set! program i (cdr (vector-ref program i))))
+             (void)))
+        (_ (void)))
+    table)))
 
 (define (build-executable program)
   (validate-program program)
@@ -68,16 +72,105 @@
          [labels (build-label-table program)])
     (cons program labels)))
 
+(define (fetch-arg instr)
+  (if (eq? (car instr) 'HALT)
+      '()
+      (second instr)))
+ 
+(define (deref-arg arg memory labels)
+  (match arg
+    ((list '= x) x)
+    ((list '* x) (hash-ref! memory (hash-ref! memory x 0) 0))
+    (x #:when (label? x)
+       (hash-ref! labels x -1))
+    (x (hash-ref! memory x 0))))
+
+(define (half-deref-arg arg memory labels)
+  (match arg
+    ((list '= x) x)
+    ((list '* x) (hash-ref! memory x 0))
+    (x #:when (label? x)
+       (hash-ref! labels x -1))
+    (x x)))
+
+(define (fetch-mnemonic instr)
+  (car instr))
+
 (define (run executable tape)
   (let ([output-tape '()]
         [labels (cdr executable)]
         [code (car executable)]
         [instr-ptr 0]
-        [stack-ptr 0]
+        [stack '()]
         [memory (make-hash)])
     (for ([i (in-range 0 (vector-length code))]
           #:break (halt? (vector-ref code i)))
-      (print "xD\n"))))
+      (let* ([instr (vector-ref code i)]
+              [arg (fetch-arg instr)]
+              [mnemonic (fetch-mnemonic instr)])
+        (match mnemonic
+          ('ADD
+           (hash-set! memory 0
+                      (+ (hash-ref! memory 0 0)
+                         (deref-arg arg memory labels))))
+          ('SUB
+           (hash-set! memory 0
+                      (- (hash-ref! memory 0 0)
+                         (deref-arg arg memory labels))))
+          ('DIV
+           (hash-set! memory 0
+                      (quotient (hash-ref! memory 0 0)
+                         (deref-arg arg memory labels))))
+          ('MULT
+           (hash-set! memory 0
+                      (* (hash-ref! memory 0 0)
+                         (deref-arg arg memory labels))))
+          ('READ
+           (hash-set! memory arg
+                      (if (null? tape)
+                          (error "No input to read!")
+                          (let ([val (car tape)])
+                            (set! tape (cdr tape))
+                            val))))
+          ('WRITE
+           (set! output-tape (cons (deref-arg arg memory labels) output-tape)))
+          ('STORE
+           (hash-set! memory arg (hash-ref! memory 0 0)))
+          ('LOAD
+           (hash-set! memory 0 (deref-arg arg memory labels)))
+          ('JUMP
+           (set! instr-ptr arg))
+          ('JZERO
+           (if (= 0 (hash-ref! memory 0 0))
+               (set! instr-ptr arg)
+               (void)))
+          ('JGTZ
+           (if (> 0 (hash-ref! memory 0 0))
+               (set! instr-ptr arg)
+               (void)))
+          ('JLTZ
+           (if (< 0 (hash-ref! memory 0 0))
+               (set! instr-ptr arg)
+               (void)))
+          ('PUSH
+           (set! stack (cons (deref-arg arg memory labels) stack)))
+          ('POP
+           (if (null? stack)
+               (error "Stack underflow!")
+               (begin
+                 (hash-set! memory (deref-arg arg memory) (car stack))
+                 (set! stack (cdr stack)))))
+          ('CALL
+           (begin
+             (set! stack (cons instr-ptr stack))
+             (set! instr-ptr arg)))
+          ('RET
+           (begin
+             (set! instr-ptr (car stack))
+             (set! stack (cdr stack))))
+          ('HALT (void))
+          )))
+    output-tape))
 
 (define (main)
   (let* ([file-to-run (command-line
@@ -89,7 +182,7 @@
                 #:mode 'text)]
          [program (read file)]
          [executable (build-executable program)])
-    (run executable '())
+    (printf "~a\n" (run executable (read)))
     (close-input-port file)))
 
 (main)
